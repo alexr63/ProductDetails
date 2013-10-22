050a475dca2268d5392083083ec28ec9839cc3b9
alexr63 <alex@resnik.co.ua>
2013-10-22 13:45:44 +0300

    ISearchable (not tested)

---
+++ Components/ProductDetailsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Web;
+using DotNetNuke;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Services.Search;
+using ProductList;
+
+namespace Cowrie.ProductDetails.Components
+{
+    public class ProductDetailsController : ISearchable
+	{
+		#region ISearchable Members
+
+		/// <summary>
+		/// Implements the search interface required to allow DNN to index/search the content of your
+		/// module
+		/// </summary>
+		/// <param name="modInfo"></param>
+		/// <returns></returns>
+		public DotNetNuke.Services.Search.SearchItemInfoCollection GetSearchItems(ModuleInfo modInfo)
+		{
+			SearchItemInfoCollection searchItems = new SearchItemInfoCollection();
+		    using (SelectedHotelsEntities db = new SelectedHotelsEntities())
+		    {
+                IList<Hotel> hotels = (from p in db.Products
+                                       where !p.IsDeleted
+                                       select p).OfType<Hotel>().ToList();
+		        foreach (var hotel in hotels)
+		        {
+                    SearchItemInfo searchInfo = new SearchItemInfo(hotel.Name, hotel.Description, hotel.CreatedByUser, hotel.CreatedDate,
+                                                        modInfo.ModuleID, hotel.Id.ToString(), hotel.Description, "Item=" + hotel.Id.ToString());
+                    searchItems.Add(searchInfo);
+                }
+            }
+
+			return searchItems;
+		}
+
+		#endregion
+	}
+}
